93f67b59051838a94ae9b00ab85bcff9f08a0662
rionika <91502342+rionika@users.noreply.github.com>
2021-09-28 03:07:33 +0600

    Add files via upload

---
+++ de.cpp
@@ -0,0 +1,45 @@
+#include "base64.h"
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+
+
+
+int main(){
+
+
+//int clave;
+string msg;
+
+	 msg = "VWxKMU0waEAhiWExsb1kZVSkJVaV29saRlRVSjZ2TVRGZVhOa1JGWkdOb1pXRlpRVWhOVkE9PQ==";
+	
+
+	//auto nuevo = base64::Cifrar(msg);
+	//cout << "\nEncoded message: " << nuevo<<endl;
+	
+	msg.erase(10,1).erase(18,1).erase(24,1).erase(27,1);
+	cout <<"1st subtraction\n"<< msg<<endl;
+	
+	auto nuevo = base64::Descifrar(msg);
+	msg = nuevo; 
+	
+	cout << "\n1st Decoded message:\n" << msg<<endl;
+
+
+	msg.erase(10,1).erase(18,1).erase(24,1).erase(27,1);
+	cout<<"\n2nd subtraction:\n"<<msg<<endl;
+	
+	cout<<"Give the code: ";
+	cin>> ws;
+	getline(cin, msg);
+	
+auto nuevo1 = base64::Descifrar(msg);
+	msg=nuevo1;
+	
+cout << "\nOrginal code: \n" <<msg<<endl;
+
+
+
+return 0;
+}
---
+++ ed.cpp
@@ -0,0 +1,29 @@
+#include "base64.h"
+#include <iostream>
+#include <algorithm>
+#include <string>
+int main(){
+
+
+	string msg = "RRu3HGmyhUBAQfEMBz11W6DEdcheaYAHMT";
+	
+	//auto nuevo = base64::Cifrar(msg);
+	//cout << "\nEncoded message: " << nuevo<<endl;
+	
+	auto nuevo = base64::Cifrar(msg);
+	string bs = nuevo; 
+		cout << "\n1st Encoded message:\n" << bs<<endl;
+		bs.insert(10, "L").insert(19,"o").insert(26,"v").insert(30,"e");
+		cout <<"1st addition \n"<< bs;
+	
+	auto nuevo1 = base64::Cifrar(bs);
+	bs = nuevo1; 
+	
+	cout << "\n2nd Encoded message:\n" << bs<<endl;
+	bs.insert(10, "A").insert(19,"k").insert(26,"a").insert(30,"s");
+	cout << "\nFinal code:\n"<<bs<<endl;
+
+
+
+return 0;
+}
