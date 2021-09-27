3bef83743dc20c3bb2033908a50632929731fbad
rionika <91502342+rionika@users.noreply.github.com>
2021-09-28 03:10:56 +0600

    Update de.cpp

--- de.cpp
+++ de.cpp
@@ -14,10 +14,6 @@ string msg;
 
 	 msg = "kkjsdkjfksajfksdj==";
 	
-
-	//auto nuevo = base64::Cifrar(msg);
-	//cout << "\nEncoded message: " << nuevo<<endl;
-	
 	msg.erase(10,1).erase(18,1).erase(24,1).erase(27,1);
 	cout <<"1st subtraction\n"<< msg<<endl;
 	
@@ -37,7 +33,7 @@ string msg;
 auto nuevo1 = base64::Descifrar(msg);
 	msg=nuevo1;
 	
-cout << "\nOrginal code: \n" <<msg<<endl;
+cout << "\nFinal code: \n" <<msg<<endl;
 
 
 
